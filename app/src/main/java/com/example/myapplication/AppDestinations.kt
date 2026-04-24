039420b21dfb7babc499bef69c986d764d988564
vasiliikarpenko <invweb@gmail.com>
2026-04-24 17:40:29 +0300

    work in progress

---
+++ app/src/main/java/com/example/myapplication/AppDestinations.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+enum class AppDestinations(val label: Int, val icon: Int, val contentDescription: Int) {
+    HOME(R.string.home, R.drawable.ic_home, R.string.home),
+    FAVORITES(R.string.favorites, R.drawable.ic_favorite, R.string.favorites),
+    PROFILE(R.string.profile, R.drawable.ic_account_box, R.string.profile)
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,11 +16,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,18 +43,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApplicationApp() {
     var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
-
     NavigationSuiteScaffold(
         navigationSuiteItems = {
             AppDestinations.entries.forEach {
                 item(
                     icon = {
                         Icon(
-                            painterResource(it.icon),
-                            contentDescription = it.label
+                            ImageVector.vectorResource(id = it.icon),
+                            contentDescription = it.label.toString()
                         )
                     },
-                    label = { Text(it.label) },
+                    label = { Text(stringResource(it.label)) },
                     selected = it == currentDestination,
                     onClick = { currentDestination = it }
                 )
@@ -63,15 +67,6 @@ fun MyApplicationApp() {
             )
         }
     }
-}
-
-enum class AppDestinations(
-    val label: String,
-    val icon: Int,
-) {
-    HOME("${R.string.home}", R.drawable.ic_home),
-    FAVORITES("${R.string.favorites}", R.drawable.ic_favorite),
-    PROFILE("${R.string.profile}", R.drawable.ic_account_box),
 }
 
 @Composable
