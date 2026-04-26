736a69ff98289d6c0b52f5d0e28188d504863cd2
vasiliikarpenko <invweb@gmail.com>
2026-04-26 12:18:56 +0300

    work in progress

--- app/src/main/java/com/example/myapplication/ui/AppDestinations.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.ui
-
-import com.example.myapplication.R
-
-enum class AppDestinations(val label: Int, val icon: Int, val contentDescription: Int) {
-    HOME(R.string.home, R.drawable.ic_home, R.string.home),
-    FAVORITES(R.string.favorites, R.drawable.ic_favorite, R.string.favorites),
-    PROFILE(R.string.profile, R.drawable.ic_account_box, R.string.profile)
-}
